527cabfd3319aae10afa1e9f1b517d0233c847da
Yolol100 <Andrew_Baeten@hotmail.com>
2026-05-24 05:33:52 +0700

    Test

--- templates/admin/partials/empty-state.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-?>
-<div class="notice notice-info inline">
-	<p><?php esc_html_e( "Er zijn nog geen items met exporteerbare velden gevonden. Het beheerscherm blijft beschikbaar; voeg content of ondersteunde velden toe en ververs daarna de pagina.", 'acf-page-text-manager' ); ?></p>
-</div>
